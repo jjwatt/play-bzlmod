7904b7bfaff830b290d9db467ccf7cff7fb4acff
Jesse Wattenbarger <jwattenb@redhat.com>
2023-09-11 12:04:44 -0400

    Add some stuff to get started with bzlmod

---
+++ __main__.py
@@ -0,0 +1,7 @@
+
+from lib import main
+import sys
+if __name__ == "__main__":
+    print(main([["A", 1], ["B", 2]]))
+    print(sys.version)
+
---
+++ lib.py
@@ -0,0 +1,4 @@
+from tabulate import tabulate
+
+def main(table):
+    return tabulate(table)
